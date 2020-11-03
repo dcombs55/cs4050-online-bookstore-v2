8f42017b812549df8b9aa9dfff54cf4b95a445e8
jps80980 <jps80980@uga.edu>
2020-11-02 20:58:23 -0500

    quick push

--- Bookstore/src/database/CreditCardEncryption.java
+++ Bookstore/src/database/CreditCardEncryption.java
@@ -1,31 +1,22 @@
 package database;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 import bean.Customer;
 
 public class CreditCardEncryption {
 	
-	private String key = "@2334dgdfg@#$%dsgdf"; 
-    private String user = ""; 
-    private String key2 = "@1567sedf#2@"; 
-    private String pass = "";
-    
-    public String getKey() {return key;} 
-    public String getUser() {return user;} 
-    public String getKey2() {return key2;} 
-    public String getPass() {return pass;} 
-	
-	public int encrypt(Customer customer) throws ClassNotFoundException{
+	public int addCreditCard(Customer customer) throws ClassNotFoundException{
+		int result = 0;
+		Class.forName("com.mysql.cj.jdbc.Driver");
 		
-		
-		catch (SQLException e) {
-			printSQLException(e);
-		}
+		try (Connection connection = DriverManager
+				.getConnection("jdbc:mysql://cs4050-online-bookstore.cmosf0873dbb.us-east-2.rds.amazonaws.com:3306/Bookstore?serverTimezone=UTC", "bookstoreAdmin", "Gogobookstore1");
+					){
+				
+			} catch (SQLException e) {
+				printSQLException(e);
+			}
 		
 		return result;
 	}
--- Bookstore/src/database/MainConnection.java
+++ Bookstore/src/database/MainConnection.java
@@ -8,7 +8,7 @@ public class MainConnection {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
